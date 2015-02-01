5765992a6f9e80d604a0968fdd6860463c5db1a1
wheleph <wheleph@gmail.com>
2015-02-01 11:39:42 +0200

    Using logger for impl

--- impl/src/main/java/wheleph/ServiceImpl.java
+++ impl/src/main/java/wheleph/ServiceImpl.java
@@ -1,14 +1,24 @@
 package wheleph;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ServiceImpl implements Service {
+    private Logger logger = LoggerFactory.getLogger(ServiceImpl.class);
+
     @Override
     public int execute(int code) {
+        logger.debug("Invoking execute(" + code + ")");
+
         if (code == CODE_THROW_KNOWN_EXCEPTION) {
+            logger.debug("Throwing known exception");
             throw new IllegalStateException();
         } else if (code == CODE_THROW_UNKNOWN_EXCEPTION) {
+            logger.debug("Throwing unknown exception");
             throw new ServiceImplInternalException();
         }
 
+        logger.debug("Returning a value");
         return 10;
     }
 }
