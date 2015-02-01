3353b0f5f8e1e83fd434bafe1791c97a76cd53c6
wheleph <wheleph@gmail.com>
2015-02-01 11:25:50 +0200

    Setting up test sample in a common method

--- camel-client/src/test/java/wheleph/CamelClientTest.java
+++ camel-client/src/test/java/wheleph/CamelClientTest.java
@@ -9,27 +9,32 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import static wheleph.Service.*;
 
 public class CamelClientTest extends CamelSpringTestSupport {
+    private Service service;
+
     @Override
     protected AbstractApplicationContext createApplicationContext() {
         return new ClassPathXmlApplicationContext("META-INF/spring/camel-client.xml");
     }
 
+    @Override
+    protected void doPostSetup() throws Exception {
+        super.doPostSetup();
+
+        service = context.getRegistry().lookupByNameAndType("serviceProxy", Service.class);
+    }
+
     @Test
     public void testNormal() {
-        Service service = context.getRegistry().lookupByNameAndType("serviceProxy", Service.class);
-
         Assert.assertEquals(10, service.execute(CODE_SUCCESS));
     }
 
     @Test(expected = IllegalStateException.class)
     public void testKnownException() {
-        Service service = context.getRegistry().lookupByNameAndType("serviceProxy", Service.class);
         service.execute(CODE_THROW_KNOWN_EXCEPTION);
     }
 
     @Test(expected = Exception.class)
     public void testUnknownException() {
-        Service service = context.getRegistry().lookupByNameAndType("serviceProxy", Service.class);
         service.execute(CODE_THROW_UNKNOWN_EXCEPTION);
     }
 }
